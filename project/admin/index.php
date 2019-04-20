19d189a9e8460f6192cc6fe99077773527d13511
Deep Panchal <panchaldeep009@gmail.com>
2019-04-20 01:29:07 -0400

    Add Access-Control-Allow-Origin,

--- project/admin/index.php
+++ project/admin/index.php
@@ -1,58 +1,59 @@
 <?php
 
-    require_once('../connect.php');
-    require_once('functions.php');
+require_once('../connect.php');
+require_once('functions.php');
+header('Access-Control-Allow-Origin: *');
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        if(checkLogin()){
-            $response = $_SESSION['user'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (checkLogin()) {
+        $response = $_SESSION['user'];
+    } else {
+        $response = array('error' => "No login user found");
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['login'])) {
+        if (checkLogin()) {
+            $response = array('success' => 'A user already logged in');
         } else {
-            $response = array('error' => "No login user found");
-        }
-    } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if(isset($_POST['login'])){
-            if(checkLogin()){
-                $response = array('success' => 'A user already logged in');
+            if (isset($_POST['username'], $_POST['password'])) {
+                $response = login($_POST['username'], $_POST['password'], $pdo);
             } else {
-                if(isset($_POST['username'], $_POST['password'])){
-                    $response = login($_POST['username'], $_POST['password'],$pdo);
-                } else {
-                    $response = array('error' => 'Bad Request');
-                }
+                $response = array('error' => 'Bad Request');
             }
-        } else if(isset($_POST['create_user'])) {
-            if(checkLogin() && $_SESSION['user']['user_username'] === 'Admin'){
-                if(isset($_POST['username'], $_POST['password'], $_POST['name'], $_POST['email'])){
-                    $response = create_user($_POST['username'], $_POST['password'], $_POST['name'], $_POST['email'], $pdo);
-                } else {
-                    $response = array('error' => 'Bad Request');
-                }
-            } else {
-                $response = array('error' => 'Admin must be logged in');
-            }
-        } else if(isset($_POST['log_out'])){
-            $response = logout();
-        } else {
-            $response = array('error' => "Invalid Request");
         }
-    } else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-        $putF = fopen('php://input', 'r');
-        $putSTR = '';
-        while($data = fread($putF, 1024))
-            $putSTR.= $data;
-        fclose($putF);
-        parse_str($putSTR, $_PUT);
-
-        if(checkLogin()){
-            if(isset($_PUT['username'], $_PUT['password'], $_PUT['name'], $_PUT['email'])){
-                $response = edit_user($_PUT['username'], $_PUT['email'], $_PUT['name'], $_PUT['password'], $pdo);
+    } else if (isset($_POST['create_user'])) {
+        if (checkLogin() && $_SESSION['user']['user_username'] === 'Admin') {
+            if (isset($_POST['username'], $_POST['password'], $_POST['name'], $_POST['email'])) {
+                $response = create_user($_POST['username'], $_POST['password'], $_POST['name'], $_POST['email'], $pdo);
             } else {
                 $response = array('error' => 'Bad Request');
             }
         } else {
-            $response = array('error' => 'User must be logged in');
+            $response = array('error' => 'Admin must be logged in');
         }
+    } else if (isset($_POST['log_out'])) {
+        $response = logout();
+    } else {
+        $response = array('error' => "Invalid Request");
     }
+} else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $putF = fopen('php://input', 'r');
+    $putSTR = '';
+    while ($data = fread($putF, 1024))
+        $putSTR .= $data;
+    fclose($putF);
+    parse_str($putSTR, $_PUT);
 
-    header('Content-type: application/json');
-    echo json_encode( $response );
+    if (checkLogin()) {
+        if (isset($_PUT['username'], $_PUT['password'], $_PUT['name'], $_PUT['email'])) {
+            $response = edit_user($_PUT['username'], $_PUT['email'], $_PUT['name'], $_PUT['password'], $pdo);
+        } else {
+            $response = array('error' => 'Bad Request');
+        }
+    } else {
+        $response = array('error' => 'User must be logged in');
+    }
+}
+
+header('Content-type: application/json');
+echo json_encode($response);
--- project/index.php
+++ project/index.php
@@ -1,92 +1,85 @@
 <?php
 
-    require_once('connect.php');
-    require_once('includes/products.php');
-    require_once('includes/categories.php');
-    require_once('admin/functions.php');
-    
-    // Admin Requests
-    
-    // Content Requests
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        if(isset($_GET['catagories'])){
-            $response = get_categories($pdo);
-        } else if(isset($_GET['product_id'])){
-            $response = get_product($pdo, $_GET);
-        } else {
-            $response = get_all_products_name($pdo, $_GET);
-        }
-    } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+require_once('connect.php');
+require_once('includes/products.php');
+require_once('includes/categories.php');
+require_once('admin/functions.php');
+header('Access-Control-Allow-Origin: *');
+// Admin Requests
 
-        if(checkLogin()){
-            if(isset($_POST['product_name'])){
-                $response = create_product($pdo, $_POST);
-
-            } else if(isset($_POST['category_name'])){
-                $response = create_category($pdo, $_POST);
-
-            } else if(isset($_POST['category_id'], $_POST['product_id'])){
-                $response = create_product_category($pdo, $_POST);
-
-            } else if(isset($_POST['product_id'], $_FILES['photo'])){
-                $response = create_product_media($pdo, $_POST, $_FILES);
-            } else {
-                $response = array('error' => 'Bad Request');
-            }
-        } else {
-            $response = array('error' => 'User must be logged in');
-        }
-
-    } else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-
-        $putF = fopen('php://input', 'r');
-        $putSTR = '';
-        while($data = fread($putF, 1024))
-            $putSTR.= $data;
-        fclose($putF);
-        parse_str($putSTR, $_PUT);
-
-        if(checkLogin()){
-
-            if(isset($_PUT['product_id'])){
-                $response = edit_product_name($pdo, $_PUT);
-            } else if(isset($_PUT['information_id'])){
-                $response = edit_product_info($pdo, $_PUT);
-            } else if(isset($_PUT['products_category_id'])){
-                $response = edit_product_category($pdo, $_PUT);
-            } else if(isset($_PUT['media_id'])){
-                $response = edit_product_media($pdo, $_PUT, $_FILES);
-            } else {
-                $response = array('error' => 'Bad Request');
-            }
-        } else {
-            $response = array('error' => 'User must be logged in');
-        }
-
-    } else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-
-        $putF = fopen('php://input', 'r');
-        $putSTR = '';
-        while($data = fread($putF, 1024))
-            $putSTR.= $data;
-        fclose($putF);
-        parse_str($putSTR, $_DELETE);
-
-        if(checkLogin()){
-
-            if(isset($_PUT['product_id'])){
-                $response = delete_product($pdo, $_DELETE);
-            } else if(isset($_PUT['products_category_id'])){
-                $response = delete_product_category($pdo, $_DELETE);
-            } else if(isset($_PUT['media_id'])){
-                $response = delete_product_media($pdo, $_DELETE);
-            } else {
-                $response = array('error' => 'Bad Request');
-            }
-        } else {
-            $response = array('error' => 'User must be logged in');
-        }
-
+// Content Requests
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['catagories'])) {
+        $response = get_categories($pdo);
+    } else if (isset($_GET['product_id'])) {
+        $response = get_product($pdo, $_GET);
+    } else {
+        $response = get_all_products_name($pdo, $_GET);
     }
-    header('Content-type: application/json');
-    echo json_encode( $response );
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (checkLogin()) {
+        if (isset($_POST['product_name'])) {
+            $response = create_product($pdo, $_POST);
+        } else if (isset($_POST['category_name'])) {
+            $response = create_category($pdo, $_POST);
+        } else if (isset($_POST['category_id'], $_POST['product_id'])) {
+            $response = create_product_category($pdo, $_POST);
+        } else if (isset($_POST['product_id'], $_FILES['photo'])) {
+            $response = create_product_media($pdo, $_POST, $_FILES);
+        } else {
+            $response = array('error' => 'Bad Request');
+        }
+    } else {
+        $response = array('error' => 'User must be logged in');
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+    $putF = fopen('php://input', 'r');
+    $putSTR = '';
+    while ($data = fread($putF, 1024))
+        $putSTR .= $data;
+    fclose($putF);
+    parse_str($putSTR, $_PUT);
+
+    if (checkLogin()) {
+
+        if (isset($_PUT['product_id'])) {
+            $response = edit_product_name($pdo, $_PUT);
+        } else if (isset($_PUT['information_id'])) {
+            $response = edit_product_info($pdo, $_PUT);
+        } else if (isset($_PUT['products_category_id'])) {
+            $response = edit_product_category($pdo, $_PUT);
+        } else if (isset($_PUT['media_id'])) {
+            $response = edit_product_media($pdo, $_PUT, $_FILES);
+        } else {
+            $response = array('error' => 'Bad Request');
+        }
+    } else {
+        $response = array('error' => 'User must be logged in');
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+    $putF = fopen('php://input', 'r');
+    $putSTR = '';
+    while ($data = fread($putF, 1024))
+        $putSTR .= $data;
+    fclose($putF);
+    parse_str($putSTR, $_DELETE);
+
+    if (checkLogin()) {
+
+        if (isset($_PUT['product_id'])) {
+            $response = delete_product($pdo, $_DELETE);
+        } else if (isset($_PUT['products_category_id'])) {
+            $response = delete_product_category($pdo, $_DELETE);
+        } else if (isset($_PUT['media_id'])) {
+            $response = delete_product_media($pdo, $_DELETE);
+        } else {
+            $response = array('error' => 'Bad Request');
+        }
+    } else {
+        $response = array('error' => 'User must be logged in');
+    }
+}
+header('Content-type: application/json');
+echo json_encode($response);
