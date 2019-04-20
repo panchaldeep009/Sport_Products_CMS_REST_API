9338711e3d09cbb8f27bac1c0227d80ffae0c719
Deep Panchal <panchaldeep009@gmail.com>
2019-04-19 21:32:16 -0400

    Edit and Delete api calls added

--- project/includes/products.php
+++ project/includes/products.php
@@ -1,5 +1,7 @@
 <?php
-    // Get Products
+
+    // Get Products functions
+
     function get_all_products_name($pdo, $REQUEST){
 
         /// Pagination
@@ -67,30 +69,6 @@
         );
     }
 
-    function post_products_name($pdo, $REQUEST){
-    
-        if(isset($REQUEST['filter'])){
-            $request = $pdo->prepare('SELECT * FROM tbl_products WHERE product_id in (SELECT product_id FROM tbl_products_categories WHERE category_id in ('.$REQUEST['filter'].'))');
-        } else {
-            $request = $pdo->prepare('SELECT * FROM tbl_products');
-        }
-        
-        $request->execute();
-        $products = $request->fetchAll();
-    
-        if(isset($REQUEST['search'])){
-            $products = array_filter($products, function ($product) use ($REQUEST){
-                if (strpos(strtolower($product['product_name']), strtolower($REQUEST['search'])) !== false) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-        }
-    
-        return $products;
-    }
-
     function get_product($pdo, $REQUEST){
 
         $request = $pdo->prepare('SELECT * FROM tbl_products WHERE product_id = '.$REQUEST['product_id']);
@@ -104,6 +82,14 @@
         $product['catagories'] = [];
         foreach( $catagories as $category ) {
             $product['catagories'][] = $category;
+        }
+        
+        $request = $pdo->prepare('SELECT * FROM tbl_products_categories WHERE product_id = '.$product['product_id']);
+        $request->execute();
+        $catagories = $request->fetchAll();
+        $product['catagories_id'] = [];
+        foreach( $catagories as $category ) {
+            $product['catagories_id'][] = $category;
         }
     
         $request = $pdo->prepare('SELECT * FROM tbl_products_information WHERE product_id = '.$product['product_id']);
@@ -120,11 +106,13 @@
         $medias = $request->fetchAll();
         $product['media'] = [];
         foreach ($medias as $media) {
-            $product['media'][] = $media['media_src'];
+            $product['media'][] = $media;
         }
     
         return $product;
     }
+
+    /// Create Products functions
 
     function create_product($pdo, $REQUEST){
 
@@ -217,6 +205,222 @@
             return array('success' => "Product Created");
         } else {
             return array('error' => "No admin logged in");
+        }
+    }
+
+    function create_category($pdo, $REQUEST){
+
+        $create_category_query = 'INSERT INTO `tbl_categories` (`category_name`) VALUES (:category_name);';
+
+        $create_category = $pdo->prepare($create_category_query); 
+        $create_category->bindParam(':category_name', $REQUEST['category_name']);
+        $create_category->execute();
+
+        if($create_category->rowCount() > 0){
+            return array('success' => "Category inserted");
+        } else {
+            return array('error' => "Unable to create category");
+        }
+    }
+
+    function create_product_category($pdo, $REQUEST){
+        $create_category_query = 'INSERT INTO `tbl_products_categories` (`category_id`, `product_id`) VALUES (:category_id, :product_id);';
+
+        $create_category = $pdo->prepare($create_category_query); 
+        $create_category->bindParam(':category_id', intval($REQUEST['category_id']));
+        $create_category->bindParam(':product_id', intval($REQUEST['product_id']));
+        $create_category->execute();
+        if($create_category->rowCount() > 0){
+            return array('success' => "Product category inserted");
+        } else {
+            return array('error' => "Unable to insert product category");
+        }
+    }
+
+    function create_product_media($pdo, $REQUEST, $FILES){
+        $product_id = $REQUEST['product_id'];
+
+        if(isset($FILES['photo'])){
+            if(getimagesize($FILES["photo"]["tmp_name"]) !== false){
+                $thisImageSource = 'images/image_'.$product_id.'_'.$FILES["photo"]["name"];
+
+                if (move_uploaded_file($FILES["photo"]["tmp_name"], $thisImageSource)) {
+                    $create_media_query = 'INSERT INTO `tbl_products_media` (`product_id`, `media_src`) VALUES (:product_id, :media_src);';
+
+                    $create_media = $pdo->prepare($create_media_query); 
+                    $create_media->bindParam(':product_id', intval($product_id));
+                    $create_media->bindParam(':media_src', $thisImageSource);
+                    $create_media->execute();
+    
+                    if(!$create_media->rowCount() > 0){
+                        return array('error' => "Unable to Upload Image");
+                    } else {
+                        return array('success' => "Image upload successfully");
+                    }
+                } else {
+                    return array('error' => "Unable to Upload Image");
+                }
+            } else {
+                return array('error' => "Uploaded File is not image");
+            }
+        }
+    }
+
+    /// Edit Products functions
+
+    function edit_product_name($pdo, $REQUEST){
+        $edit_product_query = "UPDATE `tbl_products` SET `product_name` = :product_name WHERE `product_id` = :product_id;";
+
+        $edit_product = $pdo->prepare($edit_product_query); 
+        $edit_product->bindParam(':product_name', $REQUEST['product_name']);
+        $edit_product->bindParam(':product_id', $REQUEST['product_id']);
+        $edit_product->execute();
+
+        if($edit_product->rowCount() > 0){
+            return array('success' => "Product edited successfully");
+        } else {
+            return array('error' => "Unable to edit product");
+        }
+    }
+
+    function edit_product_info($pdo, $REQUEST){
+        $edit_product_query = "UPDATE `tbl_products_information` SET `description` = :description, `features` = :features, price = :price WHERE `information_id` = :information_id;";
+
+        $edit_product = $pdo->prepare($edit_product_query); 
+        $edit_product->bindParam(':information_id', $REQUEST['information_id']);
+        $edit_product->bindParam(':description', $REQUEST['description']);
+        $edit_product->bindParam(':features', $REQUEST['features']);
+        $edit_product->bindParam(':price', $REQUEST['price']);
+        $edit_product->execute();
+
+        if($edit_product->rowCount() > 0){
+            return array('success' => "Product information edited successfully");
+        } else {
+            return array('error' => "Unable to edit product information");
+        }
+    }
+
+    function edit_product_category($pdo, $REQUEST){
+        $edit_product_category_query = "UPDATE `tbl_products_categories` SET `category_id` = :category_id WHERE `category_id` = :category_id AND `product_id` = :product_id;";
+
+        $edit_product_category = $pdo->prepare($edit_product_category_query); 
+        $edit_product_category->bindParam(':category_id', $REQUEST['category_id']);
+        $edit_product_category->bindParam(':product_id', $REQUEST['product_id']);
+        $edit_product_category->execute();
+
+        if($edit_product_category->rowCount() > 0){
+            return array('success' => "Product category edited successfully");
+        } else {
+            return array('error' => "Unable to edit product category");
+        }
+    }
+
+    function edit_product_media($pdo, $REQUEST, $FILES){
+        $select_product_media_query = "SELECT * FROM `tbl_products_media` WHERE `media_id` = :media_id";
+        $select_product_media = $pdo->prepare($select_product_media_query); 
+        $select_product_media->bindParam(':media_id', $REQUEST['media_id']);
+        $select_product_media->execute();
+
+        $this_media = $select_product_media->fetch(PDO::FETCH_ASSOC);
+
+        if(isset($FILES['photo'])){
+            if(getimagesize($FILES["photo"]["tmp_name"]) !== false){
+
+                $newImageSource = 'images/image_'.$REQUEST['product_id'].'_'.$FILES["photo"]["name"];
+
+                if (move_uploaded_file($FILES["photo"]["tmp_name"], $newImageSource)) {
+
+                    $edit_product_media_query = "UPDATE `tbl_products_media` SET `media_src` = :media_src WHERE `media_id` = :media_id";
+
+                    $edit_product_media = $pdo->prepare($edit_product_media_query); 
+                    $edit_product_media->bindParam(':media_id', $REQUEST['media_id']);
+                    $edit_product_media->bindParam(':media_src', $newImageSource);
+                    $edit_product_media->execute();
+
+                    if($edit_product_media->rowCount() > 0){
+                        return array('success' => "Product category edited successfully");
+                        unlink($this_media['media_src']);
+                    } else {
+                        return array('error' => "Unable to edit product category");
+                        unlink($newImageSource);
+                    }
+                } else {
+                    return array('error' => "Unable to Upload Image");
+                }
+            } else {
+                return array('error' => "Uploaded File is not image");
+            }
+        }
+    }
+
+    /// Delete Products functions
+
+    function delete_product($pdo, $REQUEST){
+
+        $delete_product_query = "DELETE FROM `tbl_products` WHERE `product_id` = :product_id;";
+        $delete_product = $pdo->prepare($delete_product_query);
+        $delete_product->bindParam(':product_id', $REQUEST['product_id']);
+        $delete_product->execute();
+
+        $delete_product_info_query = "DELETE FROM `tbl_products_information` WHERE `product_id` = :product_id;";
+        $delete_product_info = $pdo->prepare($delete_product_info_query);
+        $delete_product_info->bindParam(':product_id', $REQUEST['product_id']);
+        $delete_product_info->execute();
+
+        $delete_product_category_query = "DELETE FROM `tbl_products_categories` WHERE `product_id` = :product_id;";
+        $delete_product_category = $pdo->prepare($delete_product_category_query);
+        $delete_product_category->bindParam(':product_id', $REQUEST['product_id']);
+        $delete_product_category->execute();
+
+        $delete_product_media_query = "DELETE FROM `tbl_products_media` WHERE `product_id` = :product_id;";
+        $delete_product_media = $pdo->prepare($delete_product_media_query);
+        $delete_product_media->bindParam(':product_id', $REQUEST['product_id']);
+        $delete_product_media->execute();
+
+
+        if($delete_product->rowCount() > 0){
+            return array('success' => "Product deleted successfully");
+        } else {
+            return array('error' => "Unable to deleted product");
+        }
+    }
+
+    function delete_product_category($pdo, $REQUEST){
+
+        $delete_product_category_query = "DELETE FROM `tbl_products_categories` WHERE `products_category_id` = :products_category_id;";
+        $delete_product_category = $pdo->prepare($delete_product_category_query);
+        $delete_product_category->bindParam(':products_category_id', $REQUEST['products_category_id']);
+        $delete_product_category->execute();
+
+        if($delete_product_category->rowCount() > 0){
+            return array('success' => "Product category deleted successfully");
+        } else {
+            return array('error' => "Unable to deleted product category");
+        }
+    }
+
+    function delete_product_media($pdo, $REQUEST){
+
+        $product_media = "SELECT count(*) FROM `tbl_products_media` WHERE `product_id` = ".$REQUEST['product_media'];
+        $product_media = $pdo->prepare($product_media); 
+        $product_media->execute(); 
+        $number_of_media = $product_media->fetchColumn(); 
+        
+        if($number_of_media > 1){
+    
+            $delete_product_media_query = "DELETE FROM `tbl_products_media` WHERE `media_id` = :media_id;";
+            $delete_product_media = $pdo->prepare($delete_product_media_query);
+            $delete_product_media->bindParam(':media_id', $REQUEST['media_id']);
+            $delete_product_media->execute();
+    
+            if($delete_product_media->rowCount() > 0){
+                return array('success' => "Product media deleted successfully");
+            } else {
+                return array('error' => "Unable to deleted product media");
+            }
+
+        } else {
+            return array('error' => "This is only image, upload another to delete this photo");
         }
     }
 ?>
--- project/index.php
+++ project/index.php
@@ -11,25 +11,82 @@
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if(isset($_GET['catagories'])){
             $response = get_categories($pdo);
+        } else if(isset($_GET['product_id'])){
+            $response = get_product($pdo, $_GET);
         } else {
             $response = get_all_products_name($pdo, $_GET);
         }
     } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if(isset($_POST['product_id'])){
-            $response = get_product($pdo, $_POST);
-        } else if(isset($_POST['product_name'])){
-            $response = create_product($pdo, $_POST);
+
+        if(checkLogin()){
+            if(isset($_POST['product_name'])){
+                $response = create_product($pdo, $_POST);
+
+            } else if(isset($_POST['category_name'])){
+                $response = create_category($pdo, $_POST);
+
+            } else if(isset($_POST['category_id'], $_POST['product_id'])){
+                $response = create_product_category($pdo, $_POST);
+
+            } else if(isset($_POST['product_id'], $_FILES['photo'])){
+                $response = create_product_media($pdo, $_POST, $_FILES);
+            } else {
+                $response = array('error' => 'Bad Request');
+            }
         } else {
-            $response = post_products_name($pdo, $_POST);
+            $response = array('error' => 'User must be logged in');
         }
+
     } else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-        if(isset($_POST['product_id'])){
-            $response = get_product($pdo, $_POST);
-        } else if(isset($_POST['product_name'])){
-            $response = create_product($pdo, $_POST);
+
+        $putF = fopen('php://input', 'r');
+        $putSTR = '';
+        while($data = fread($putF, 1024))
+            $putSTR.= $data;
+        fclose($putF);
+        parse_str($putSTR, $_PUT);
+
+        if(checkLogin()){
+
+            if(isset($_PUT['product_id'])){
+                $response = edit_product_name($pdo, $_PUT);
+            } else if(isset($_PUT['information_id'])){
+                $response = edit_product_info($pdo, $_PUT);
+            } else if(isset($_PUT['products_category_id'])){
+                $response = edit_product_category($pdo, $_PUT);
+            } else if(isset($_PUT['media_id'])){
+                $response = edit_product_media($pdo, $_PUT, $_FILES);
+            } else {
+                $response = array('error' => 'Bad Request');
+            }
         } else {
-            $response = post_products_name($pdo, $_POST);
+            $response = array('error' => 'User must be logged in');
         }
+
+    } else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+        $putF = fopen('php://input', 'r');
+        $putSTR = '';
+        while($data = fread($putF, 1024))
+            $putSTR.= $data;
+        fclose($putF);
+        parse_str($putSTR, $_DELETE);
+
+        if(checkLogin()){
+
+            if(isset($_PUT['product_id'])){
+                $response = delete_product($pdo, $_DELETE);
+            } else if(isset($_PUT['products_category_id'])){
+                $response = delete_product_category($pdo, $_DELETE);
+            } else if(isset($_PUT['media_id'])){
+                $response = delete_product_media($pdo, $_DELETE);
+            } else {
+                $response = array('error' => 'Bad Request');
+            }
+        } else {
+            $response = array('error' => 'User must be logged in');
+        }
+
     }
     header('Content-type: application/json');
     echo json_encode( $response );
